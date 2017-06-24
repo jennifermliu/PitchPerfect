962b7dc98dd5202ea44e16922448665ec9f2d2cd
jennifermliu <jenniferliu2018@u.northwestern.edu>
2017-06-23 23:04:39 -0500

    second view]

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -12,6 +12,24 @@ class PlaySoundsViewController: UIViewController {
 
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var SlowButton: UIButton!
+    @IBOutlet weak var FastButton: UIButton!
+    @IBOutlet weak var HighPitchButton: UIButton!
+    @IBOutlet weak var LowPitchButton: UIButton!
+    @IBOutlet weak var EchoButton: UIButton!
+    @IBOutlet weak var ReverbButton: UIButton!
+    @IBOutlet weak var StopButton: UIButton!
+    
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        print("Stop Audio Button Pressed")
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
